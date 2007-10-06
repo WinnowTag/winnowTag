020b6f3c09b42b7737e642c9c6ffa25707feaf85
seangeo <seangeo@f9e11ea3-fe04-0410-b62f-af45e3a9b5a1>
2007-10-06 00:30:30 +0000

    Removed bulk tagging model and controller - no longer used.
    git-svn-id: http://svn.winnow.peerworks.org/trunk/winnow@1870 f9e11ea3-fe04-0410-b62f-af45e3a9b5a1

---
+++ db/migrate/077_remove_bulk_taggings.rb
@@ -0,0 +1,20 @@
+# Copyright (c) 2007 The Kaphan Foundation
+#
+# Possession of a copy of this file grants no permission or license
+# to use, modify, or create derivate works.
+# Please contact info@peerworks.org for further information.
+#
+
+class RemoveBulkTaggings < ActiveRecord::Migration
+  def self.up
+    drop_table :bulk_taggings
+  end
+
+  def self.down
+    create_table :bulk_taggings do |t|
+      t.column :filter_type, :string
+      t.column :filter_value, :string
+      t.column :created_on, :datetime
+    end
+  end
+end
