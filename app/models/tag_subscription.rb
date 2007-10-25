ec0efad2dd93b571c8381cb71ce30e88cdde1d15
craigd <craigd@f9e11ea3-fe04-0410-b62f-af45e3a9b5a1>
2007-10-25 18:11:23 +0000

    added peerworks header to TagSubscription model and test
    git-svn-id: http://svn.winnow.peerworks.org/trunk/winnow@2011 f9e11ea3-fe04-0410-b62f-af45e3a9b5a1

--- app/models/tag_subscription.rb
+++ app/models/tag_subscription.rb
@@ -1,3 +1,10 @@
+# Copyright (c) 2007 The Kaphan Foundation
+#
+# Possession of a copy of this file grants no permission or license
+# to use, modify, or create derivate works.
+# Please contact info@peerworks.org for further information.
+#
+
 class TagSubscription < ActiveRecord::Base
   belongs_to :user
   belongs_to :tag
--- test/unit/tag_subscription_test.rb
+++ test/unit/tag_subscription_test.rb
@@ -1,3 +1,10 @@
+# Copyright (c) 2007 The Kaphan Foundation
+#
+# Possession of a copy of this file grants no permission or license
+# to use, modify, or create derivate works.
+# Please contact info@peerworks.org for further information.
+#
+
 require File.dirname(__FILE__) + '/../test_helper'
 
 class TagSubscriptionTest < Test::Unit::TestCase
