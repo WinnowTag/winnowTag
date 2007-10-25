195517c4d099ec08cf799f7cbecf950a8a2f3801
mark <mark@f9e11ea3-fe04-0410-b62f-af45e3a9b5a1>
2007-10-25 20:50:35 +0000

    style tweaks
    git-svn-id: http://svn.winnow.peerworks.org/trunk/winnow@2021 f9e11ea3-fe04-0410-b62f-af45e3a9b5a1

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -130,6 +130,14 @@ function updateFilterControl(control, add_url, remove_url) {
 
 function update_feed_filters(element, value) {
 	$('feed_filters').appendChild(value);
+
+	// TODO: Need to unbind these listeners...
+  // for (var i = 0, length = Event.observers.length; i < length; i++) {
+  //   var event_information = Event.observers[i];
+  //   if(event_information[0] == element) {
+  //    Event.stopObserving(event_information[0], event_information[1], event_information[2], event_information[3]);
+  //   }
+  // }
 }
 
 function clear_auto_complete(element, list) {
