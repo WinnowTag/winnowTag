ac9a05d9db16d745be2a7e491e108c5ee4afd74e
mark <mark@f9e11ea3-fe04-0410-b62f-af45e3a9b5a1>
2007-11-28 19:25:38 +0000

    #506 - Added selenium tests for subscribing to public tags
    git-svn-id: http://svn.winnow.peerworks.org/trunk/winnow@2243 f9e11ea3-fe04-0410-b62f-af45e3a9b5a1

--- test/selenium_helper.rb
+++ test/selenium_helper.rb
@@ -21,3 +21,38 @@ def assert_path(path)
   url << /(\?.*)?/.inspect[1..-2]
   assert_location "regexp:^#{url}$"
 end
+
+def see_element(*args)
+  assert_element_present "css=#{args.join}"
+end
+
+def dont_see_element(*args)
+  assert_element_not_present "css=#{args.join}"
+end
+
+def assert_element_disabled(selector)
+  see_element("#{selector}[disabled=disabled]")
+end
+
+def assert_element_enabled(selector)
+  dont_see_element("#{selector}[disabled]")
+end
+
+def valid_user_attributes(attributes = {})
+  unique_id = rand(1000)
+  { :login => "user_#{unique_id}",
+    :email => "user_#{unique_id}@example.com",
+    :password => "password",
+    :password_confirmation => "password",
+    :firstname => "John_#{unique_id}",
+    :lastname => "Doe_#{unique_id}"
+  }.merge(attributes)
+end
+
+def valid_tag_attributes(attributes = {})
+  unique_id = rand(1000)
+  {
+    :name => "Tag #{unique_id}",
+    :user_id => unique_id
+  }.merge(attributes)
+end
