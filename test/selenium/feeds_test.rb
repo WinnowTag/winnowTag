e932197ea4bc68bcaeb6a39846cebdecf4104370
mark <mark@f9e11ea3-fe04-0410-b62f-af45e3a9b5a1>
2007-11-29 20:48:01 +0000

    Converted selenium feed tests to selenium rc
    git-svn-id: http://svn.winnow.peerworks.org/trunk/winnow@2281 f9e11ea3-fe04-0410-b62f-af45e3a9b5a1

---
+++ test/selenium/feeds_test.rb
@@ -0,0 +1,80 @@
+require File.dirname(__FILE__) + '/../test_helper'
+
+class FeedsTest < Test::Unit::SeleniumTestCase
+  include SeleniumHelper
+  fixtures :users, :feeds, :view_feed_states, :excluded_feeds
+  
+  def setup
+    login
+    open feeds_path
+  end
+
+  def test_changing_always_include_view_state
+    dont_see_element "#always_include_feed_2.selected"
+    
+    click "always_include_feed_2"
+    see_element "#always_include_feed_2.selected"
+    
+    refresh_and_wait
+    see_element "#always_include_feed_2.selected"
+    
+    click "always_include_feed_2"
+    dont_see_element "#always_include_feed_2.selected"
+    
+    refresh_and_wait
+    dont_see_element "#always_include_feed_2.selected"
+  end
+  
+  def test_changing_exclude_view_state
+    dont_see_element "#exclude_feed_2.selected"
+    
+    click "exclude_feed_2"
+    see_element "#exclude_feed_2.selected"
+    
+    refresh_and_wait
+    see_element "#exclude_feed_2.selected"
+    
+    click "exclude_feed_2"
+    dont_see_element "#exclude_feed_2.selected"
+    
+    refresh_and_wait
+    dont_see_element "#exclude_feed_2.selected"
+  end
+
+  def test_globally_exclude_control_disables_view_state_controls
+    click "always_include_feed_2"
+    see_element "#always_include_feed_2.selected"
+    dont_see_element "#exclude_feed_2.selected"
+    dont_see_element "#always_include_feed_2.disabled"
+    dont_see_element "#exclude_feed_2.disabled"
+
+    click "globally_exclude_feed_2"
+    dont_see_element "#always_include_feed_2.selected"
+    dont_see_element "#exclude_feed_2.selected"
+    see_element "#always_include_feed_2.disabled"
+    see_element "#exclude_feed_2.disabled"
+
+    click "always_include_feed_2"
+    dont_see_element "#always_include_feed_2.selected"
+
+    click "exclude_feed_2"
+    dont_see_element "#exclude_feed_2.selected"
+  end
+  
+  def test_toggling_between_view_states
+    dont_see_element "#always_include_feed_2.selected"
+    dont_see_element "#exclude_feed_2.selected"
+    
+    click "always_include_feed_2"
+    see_element "#always_include_feed_2.selected"
+    dont_see_element "#exclude_feed_2.selected"
+    
+    click "exclude_feed_2"
+    dont_see_element "#always_include_feed_2.selected"
+    see_element "#exclude_feed_2.selected"
+    
+    click "always_include_feed_2"
+    see_element "#always_include_feed_2.selected"
+    dont_see_element "#exclude_feed_2.selected"
+  end
+end
