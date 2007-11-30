9adbff179e1c23117adcb967d011fc425a6c5359
mark <mark@f9e11ea3-fe04-0410-b62f-af45e3a9b5a1>
2007-11-30 18:43:07 +0000

    Sleep for this test... :/
    git-svn-id: http://svn.winnow.peerworks.org/trunk/winnow@2305 f9e11ea3-fe04-0410-b62f-af45e3a9b5a1

--- vendor/plugins/selenium_jelly/init.rb
+++ vendor/plugins/selenium_jelly/init.rb
@@ -3,8 +3,10 @@ require File.dirname(__FILE__) + '/lib/selenium/base'
 
 if RAILS_ENV == 'test'
   at_exit do
+    puts "=========== EXITING ============"
     Selenium::Browser.disconnect_all! if Selenium::Configuration.default.close_browser_on_exit?
     Selenium::Server.disconnect!
+    puts "=========== EXITED ============"
   end
   
   require 'testcase_setup_and_teardown_with_blocks'
