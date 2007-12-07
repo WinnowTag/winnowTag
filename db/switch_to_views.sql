e70fd1a04725d2b3f2298c48dd7b0ec0c8dffccf
seangeo <seangeo@f9e11ea3-fe04-0410-b62f-af45e3a9b5a1>
2007-12-07 04:27:03 +0000

    Remvoed feed item tokens containers from switch to view script.
    git-svn-id: http://svn.winnow.peerworks.org/trunk/winnow@2376 f9e11ea3-fe04-0410-b62f-af45e3a9b5a1

--- db/switch_to_views.sql
+++ db/switch_to_views.sql
@@ -4,7 +4,6 @@ create or replace algorithm=merge view feed_items as select * from collector.fee
 create or replace algorithm=merge view feed_item_contents as select * from collector.feed_item_contents;
 create or replace algorithm=merge view feed_item_xml_data as select * from collector.feed_item_xml_data;
 create or replace algorithm=merge view feed_xml_datas as select * from collector.feed_xml_datas;
-create or replace algorithm=merge view feed_item_tokens_containers as select * from collector.feed_item_tokens_containers;
 create or replace algorithm=merge view random_backgrounds as select * from collector.random_backgrounds;
 create or replace algorithm=merge view tokens as select * from collector.tokens;
 
