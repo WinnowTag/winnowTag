4d9035ae6d603e950f8e76fa2b46d35caf520349
mark <mark@f9e11ea3-fe04-0410-b62f-af45e3a9b5a1>
2007-12-20 21:26:53 +0000

    Cleaning up unused code
    git-svn-id: http://svn.winnow.peerworks.org/trunk/winnow@2495 f9e11ea3-fe04-0410-b62f-af45e3a9b5a1

--- config/initializers/etc.rb
+++ config/initializers/etc.rb
@@ -8,7 +8,6 @@ ExceptionNotifier.exception_recipients = %w(wizzadmin@peerworks.org)
 ExceptionNotifier.email_prefix = "[WINNOW] "
 ExceptionNotifier.sender_address = %("Winnow Admin" <wizzadmin@peerworks.org>)
 
-require 'array_ext'
 require 'hash_ext'
 require 'module_ext'
 
