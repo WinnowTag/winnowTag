392761aee8b850c2b76436104b1b34f4010e1c47
mark <mark@f9e11ea3-fe04-0410-b62f-af45e3a9b5a1>
2008-01-10 22:51:49 +0000

    Moved notifier test to rspec
    git-svn-id: http://svn.winnow.peerworks.org/trunk/winnow@2594 f9e11ea3-fe04-0410-b62f-af45e3a9b5a1

--- spec/models/notifier_spec.rb
+++ spec/models/notifier_spec.rb
@@ -1,11 +1,12 @@
-require File.dirname(__FILE__) + '/../test_helper'
+# Copyright (c) 2007 The Kaphan Foundation
+#
+# Possession of a copy of this file grants no permission or license
+# to use, modify, or create derivate works.
+# Please contact info@peerworks.org for further information.
+#
+require File.dirname(__FILE__) + '/../spec_helper'
 
-class NotifierTest < Test::Unit::TestCase
-  FIXTURES_PATH = File.dirname(__FILE__) + '/../fixtures' unless defined?(FIXTURES_PATH)
-  CHARSET = "utf-8" unless defined?(CHARSET)
-
-  include ActionMailer::Quoting
-
+describe Notifier do
   def setup
     ActiveRecord::Base.logger.info("NotifierTest")
     ActionMailer::Base.delivery_method = :test
@@ -13,7 +14,7 @@ class NotifierTest < Test::Unit::TestCase
     ActionMailer::Base.deliveries = []
 
     @expected = TMail::Mail.new
-    @expected.set_content_type "text", "plain", { "charset" => CHARSET }
+    @expected.set_content_type "text", "plain", { "charset" => "utf-8" }
     @expected.mime_version = '1.0'
   end
 
@@ -21,18 +22,19 @@ class NotifierTest < Test::Unit::TestCase
     @expected.subject = '[DEPLOYMENT] r666 deployed'
     @expected.from    = "wizzadmin@peerworks.org"
     @expected.to      = "wizzadmin@peerworks.org"
-    @expected.body    = read_fixture('deployed')
+    @expected.body    = <<-EOMAIL
+Hello Peerworks folk,
+
+Revision 666 of "the beast" has just be deployed to  by .
+
+Comment: 
+
+Regards,
+
+Winnow Deployment Notifier
+EOMAIL
     @expected.date    = Time.now
 
     assert_equal @expected.encoded, Notifier.create_deployed("", "the beast", "666", "", "", @expected.date).encoded
   end
-
-  private
-    def read_fixture(action)
-      IO.readlines("#{FIXTURES_PATH}/notifier/#{action}")
-    end
-
-    def encode(subject)
-      quoted_printable(subject, CHARSET)
-    end
 end
