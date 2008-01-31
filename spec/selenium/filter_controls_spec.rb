376acfaf2da3712d56ac78d8ba8e06700b96f6b8
mark <mark@f9e11ea3-fe04-0410-b62f-af45e3a9b5a1>
2008-01-31 20:00:25 +0000

    Tests for sidebar filter controls
    git-svn-id: http://svn.winnow.peerworks.org/trunk/winnow@2784 f9e11ea3-fe04-0410-b62f-af45e3a9b5a1

--- spec/selenium/filter_controls_spec.rb
+++ spec/selenium/filter_controls_spec.rb
@@ -4,7 +4,48 @@ describe "filter controls" do
   fixtures :users
 
   before(:each) do
+    @user = users(:quentin)
     login
     open feed_items_path
+  end
+  
+  describe "show all items" do
+    it "is selected by default" do
+      see_element "#show_all.selected"
+    end
+  end
+  
+  describe "folders" do
+    it "is closed by default" do
+      assert_not_visible "css=#folder_tags .filter_list"
+      assert_not_visible "css=#folder_feeds .filter_list"
+    end
+    
+    it "opens when clicked" do
+      click "css=#folder_tags .header"
+      assert_visible "css=#folder_tags .filter_list"
+      
+      click "css=#folder_feeds .header"
+      assert_visible "css=#folder_feeds .filter_list"
+    end
+  end
+  
+  describe "tags" do
+    before(:each) do
+      @rails_tag = Tag.create! :user => @user, :name => "rails"
+    end
+    
+    after(:each) do
+      @rails_tag.destroy
+    end
+    
+    it "is not selected by default" do
+      dont_see_element "#show_all.selected"
+    end
+    
+    it "is selected when clicked" do
+      click "css=#show_all.selected"
+      see_element "#show_all.selected"
+    end
   end
 end
