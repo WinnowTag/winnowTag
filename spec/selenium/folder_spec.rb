d521d3ba21666a3cf57f6a75bb79ca7bc8ab15ff
mark <mark@f9e11ea3-fe04-0410-b62f-af45e3a9b5a1>
2008-02-07 20:12:33 +0000

    #634 -- added selenium tests for creating/destroying folders
    git-svn-id: http://svn.winnow.peerworks.org/trunk/winnow@2864 f9e11ea3-fe04-0410-b62f-af45e3a9b5a1

--- spec/selenium/folder_spec.rb
+++ spec/selenium/folder_spec.rb
@@ -6,14 +6,39 @@ describe "folders" do
   before(:each) do
     Folder.delete_all
     
+    @existing_folder = Folder.create! :user_id => 1, :name => "existing folder"
+    
     login
     open feed_items_path
     wait_for_ajax
   end
   
-  it "can be created"
+  it "can be created" do
+    assert_not_visible "add_folder"
+
+    click "add_folder_link"
+    assert_visible "add_folder"
+    
+    type "folder_name", "new folder"
+    hit_enter "folder_name"
+    wait_for_ajax
+    
+    new_folder = Folder.find_by_name("new folder")
+    see_element "#folder_#{new_folder.id}"
+  end
+  
   it "can be renamed"
-  it "can be destroyed"
+  
+  it "can be destroyed" do
+    see_element "#folder_#{@existing_folder.id}"
+
+    click "css=#folder_#{@existing_folder.id} .actions .destroy"
+    get_confirmation.should == "Are you sure?"
+    wait_for_ajax
+    
+    dont_see_element "#folder_#{@existing_folder.id}"
+  end
+  
   it "can have feeds added"
   it "can have feeds removed"
   it "can have private tags added"
