f94c9ade6bdaff98022fdddfc9dbef43a4eb6229
seangeo <seangeo@f9e11ea3-fe04-0410-b62f-af45e3a9b5a1>
2008-02-12 08:03:52 +0000

    Added migration to convert unread_items records into read_items_records.
    git-svn-id: http://svn.winnow.peerworks.org/trunk/winnow@2891 f9e11ea3-fe04-0410-b62f-af45e3a9b5a1

---
+++ db/migrate/124_convert_unread_items_to_read_items.rb
@@ -0,0 +1,22 @@
+# Copyright (c) 2008 The Kaphan Foundation
+#
+# Possession of a copy of this file grants no permission or license
+# to use, modify, or create derivate works.
+# Please contact info@peerworks.org for further information.
+#
+
+class ConvertUnreadItemsToReadItems < ActiveRecord::Migration
+  def self.up
+    ReadItem.transaction do
+      execute "insert into read_items (user_id, feed_item_id, created_at) " + 
+                "(select users.id as user_id, feed_items.id as feed_item_id, UTC_TIMESTAMP() " +
+                  "from users, feed_items " +
+                  "where feed_items.id not in " +
+                    "(select feed_item_id from unread_items where user_id = users.id)" +
+                ")"
+    end
+  end
+
+  def self.down
+  end
+end
