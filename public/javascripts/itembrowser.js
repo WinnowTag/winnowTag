098646677a20782e50b588cf5eb718bce0204af6
mark <mark@f9e11ea3-fe04-0410-b62f-af45e3a9b5a1>
2008-02-15 05:31:31 +0000

    #638 - do not open line item when clicking within the tags area
    git-svn-id: http://svn.winnow.peerworks.org/trunk/winnow@2938 f9e11ea3-fe04-0410-b62f-af45e3a9b5a1

--- public/javascripts/itembrowser.js
+++ public/javascripts/itembrowser.js
@@ -882,7 +882,7 @@ ItemBrowser.prototype = {
 	},
 	
 	toggleOpenCloseItem: function(item, event) {
-	  if(event && event.element().up('.tags')) { return false; }
+	  if(event && event.element().up('.item_tags')) { return false; }
 		if($('open_' + $(item).getAttribute('id')).visible() && $('body_' + $(item).getAttribute('id')).visible()) {
 			this.closeItem(item);
 		} else {
