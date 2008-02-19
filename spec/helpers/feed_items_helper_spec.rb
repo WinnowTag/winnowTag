d91d59efb5cd634ab75335e6d685167f7118cac5
mark <mark@f9e11ea3-fe04-0410-b62f-af45e3a9b5a1>
2008-02-19 21:23:26 +0000

    Clean up feed items helper spec
    git-svn-id: http://svn.winnow.peerworks.org/trunk/winnow@2971 f9e11ea3-fe04-0410-b62f-af45e3a9b5a1

--- spec/helpers/feed_items_helper_spec.rb
+++ spec/helpers/feed_items_helper_spec.rb
@@ -10,34 +10,30 @@ require File.dirname(__FILE__) + '/../spec_helper'
 require 'tag'
 
 describe FeedItemsHelper do
+  attr_reader :current_user
   fixtures :feed_items
-  attr_reader :current_user, :session
 
-  def setup
-    @output = ""
+  before(:each) do
     @current_user = User.create! valid_user_attributes
-    @min_train_count = 1
-    @session = {}
-    @feed_item_count = nil
   end
 
-  def test_clean_html_should_strip_scripts
+  it "clean_html_should_strip_scripts" do
     assert_equal("<h1>header</h1><p>content</p>", clean_html("<script>This is a script</script><h1>header</h1><p>content</p>"))
   end
   
-  def test_clean_html_should_strip_styles
+  it "clean_html_should_strip_styles" do
     assert_equal("<h1>header</h1><p>content</p>", clean_html("<style>div {font-size:64px;}</style><h1>header</h1><p>content</p>"))
   end
   
-  def test_clean_html_should_strip_links
+  it "clean_html_should_strip_links" do
     assert_equal("<h1>header</h1><p>content</p>", clean_html('<link rel="foo"><h1>header</h1><p>content</p>'))
   end
   
-  def test_clean_html_should_strip_meta
+  it "clean_html_should_strip_meta" do
     assert_equal("<h1>header</h1><p>content</p>", clean_html('<meta http-equiv="foo"><h1>header</h1><p>content</p>'))
   end
 
-  def test_tag_controls_helper_when_untagged
+  it "tag_controls_helper_when_untagged" do
     @current_user.stub!(:tags).and_return([Tag(current_user, 'tag1'), Tag(current_user, 'tag2'), Tag(current_user, 'tag3')])
     fi = FeedItem.find(1)
     @response.body = tag_controls(fi)
@@ -45,7 +41,7 @@ describe FeedItemsHelper do
     assert_select('li.untagged', 0, @response.body)
   end
   
-  def test_tag_controls_with_classifier_tags
+  it "tag_controls_with_classifier_tags" do
     Tagging.create(:feed_item => FeedItem.find(1), :user => @current_user, :tag => Tag(current_user, 'tag1'))
     Tagging.create(:feed_item => FeedItem.find(1), :user => @current_user, :tag => Tag(current_user, 'tag2'), :classifier_tagging => true)
     @response.body = tag_controls(FeedItem.find(1))
@@ -54,7 +50,7 @@ describe FeedItemsHelper do
     assert_select('li#tag_control_for_tag2_on_feed_item_1.classifier', true)
   end
   
-  def test_tag_controls_when_negative_tagging_exists
+  it "tag_controls_when_negative_tagging_exists" do
     Tagging.create(:feed_item => FeedItem.find(1), :user => @current_user, :tag => Tag(current_user, 'tag1'), :strength => 0)
     Tagging.create(:feed_item => FeedItem.find(1), :user => @current_user, :tag => Tag(current_user, 'tag1'), :classifier_tagging => true)
     
@@ -64,7 +60,7 @@ describe FeedItemsHelper do
     assert_select('li#tag_control_for_tag1_on_feed_item_1.negative.classifier', true, @response.body)
   end
   
-  def test_tag_controls_when_positive_tagging_overrides_classifier_tagging
+  it "tag_controls_when_positive_tagging_overrides_classifier_tagging" do
     Tagging.create(:feed_item => FeedItem.find(1), :user => @current_user, :tag => Tag(current_user, 'tag1'))
     Tagging.create(:feed_item => FeedItem.find(1), :user => @current_user, :tag => Tag(current_user, 'tag1'), :classifier_tagging => true)
     @response.body = tag_controls(FeedItem.find(1))
@@ -72,29 +68,29 @@ describe FeedItemsHelper do
     assert_select('li#tag_control_for_tag1_on_feed_item_1.positive.classifier', true, @response.body)
   end
     
-  def test_clean_html_with_blank_value
+  it "clean_html_with_blank_value" do
     [nil, '', ' '].each do |value| 
       assert_nil clean_html(value)
     end
   end
   
-  def test_clean_html_with_non_blank_value
+  it "clean_html_with_non_blank_value" do
     ["string", "<div>content</div>"].each do |value| 
       assert_not_nil clean_html(value)
     end
   end
   
-  def test_feed_link_without_link
+  it "feed_link_without_link" do
     feed_item = mock_model(FeedItem, :feed => mock_model(Feed, :title => "Feed Title", :alternate => nil))
     assert_equal "Feed Title", feed_link(feed_item)
   end
   
-  def test_feed_link_with_link
+  it "feed_link_with_link" do
     feed_item = mock_model(FeedItem, :feed => mock_model(Feed, :title => "Feed Title", :alternate => "http://example.com"))
     assert_equal '<a href="http://example.com">Feed Title</a>', feed_link(feed_item)
   end
   
-  def test_feed_link_with_link_and_options
+  it "feed_link_with_link_and_options" do
     feed_item = mock_model(FeedItem, :feed => mock_model(Feed, :title => "Feed Title", :alternate => "http://example.com"))
     assert_equal '<a href="http://example.com" target="_blank">Feed Title</a>', feed_link(feed_item, :target => "_blank")
   end
