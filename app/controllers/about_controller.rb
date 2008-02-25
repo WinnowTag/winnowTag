a43e4a8724fd9485f4598f292a47680c53b47f53
mark <mark@f9e11ea3-fe04-0410-b62f-af45e3a9b5a1>
2008-02-25 00:14:26 +0000

    Test git commit
    git-svn-id: http://svn.winnow.peerworks.org/trunk/winnow@2994 f9e11ea3-fe04-0410-b62f-af45e3a9b5a1

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -3,8 +3,6 @@
 # Possession of a copy of this file grants no permission or license
 # to use, modify, or create derivate works.
 # Please contact info@peerworks.org for further information.
-#
-
 class AboutController < ApplicationController
   def index
     @about = `svn info #{__FILE__}`
