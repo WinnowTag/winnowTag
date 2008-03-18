1ac4244033cb291645b2598c0c1bb1deefe12741
Mark Van Holstyn <mvanholstyn@gmail.com>
2008-03-18 11:32:25 -0400

    Commented out about controller info from SVN. This either needs to be removed completely, or reimplemented for git

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -5,15 +5,18 @@
 # Please contact info@peerworks.org for further information.
 class AboutController < ApplicationController
   def index
-    @about = `svn info #{__FILE__}`
+    # @about = `svn info #{__FILE__}`
+    @about = "TODO: Implement from git"
     
-    if @about =~ /Revision: ([\d]+)/
-      @revision = $1
-    end
+    # if @about =~ /Revision: ([\d]+)/
+    #   @revision = $1
+    # end
+    @revision = "TODO: Implement from git"
     
-    if @about =~ /http:\/\/svn.winnow.peerworks.org\/(.+)\/app\/controllers\/about_controller.rb/
-      @repos = $1
-    end
+    # if @about =~ /http:\/\/svn.winnow.peerworks.org\/(.+)\/app\/controllers\/about_controller.rb/
+    #   @repos = $1
+    # end
+    @repos = "TODO: Implement from git"
     
     begin
       @classifier_info = Remote::Classifier.get_info
