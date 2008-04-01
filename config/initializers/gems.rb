17c14ebc73e043e9a2c5bffeee62934e4ba05a18
Sean Geoghegan <seangeo@gmail.com>
2008-04-01 14:23:34 +1030

    Require ratom in gem initializer.

--- config/initializers/gems.rb
+++ config/initializers/gems.rb
@@ -5,3 +5,6 @@ require 'hpricot'
 require 'bcrypt'
 # 3.0.4 is buggy -- Required in environment.rb so that action pack does not require 3.0.4
 # gem 'RedCloth', '3.0.3'
+
+gem 'ratom'
+require 'atom'
