f8ea2bfb2ac887657aaf19ad26c6ce20f156e446
Sean Geoghegan <seangeo@gmail.com>
2008-04-09 12:24:02 +0930

    Fixed broken tag feeds.

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -46,7 +46,7 @@ class TagsController < ApplicationController
       respond_to do |wants|
         wants.atom do        
           conditional_render([@tag.updated_on,  @tag.last_classified_at].compact.max) do
-            render :layout => false
+            render 
           end
         end
       end
--- stories/helper.rb
+++ stories/helper.rb
@@ -5,3 +5,14 @@ require 'spec/rails/story_adapter'
 def run_local_story(filename, options={})
   run File.join(File.dirname(__FILE__), filename), options
 end
+
+def valid_user_attributes(attributes = {})
+  unique_id = rand(100000)
+  { :login => "user_#{unique_id}",
+    :email => "user_#{unique_id}@example.com",
+    :password => "password",
+    :password_confirmation => "password",
+    :firstname => "John_#{unique_id}",
+    :lastname => "Doe_#{unique_id}"
+  }.merge(attributes)
+end
