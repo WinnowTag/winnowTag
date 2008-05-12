74fd163cd802dfddc46abfee7f24b0da9a53739b
Mark Van Holstyn <mvanholstyn@gmail.com>
2008-05-12 11:17:28 -0400

    Make sure only the proper users can edit/delete comments

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -24,4 +24,37 @@ describe Comment do
       Generate.comment.should belong_to(:tag)
     end
   end
+  
+  describe ".find_for_user" do
+    before(:each) do
+      @tagger = User.create! valid_user_attributes
+      @tag = Tag.create! :user_id => @tagger.id, :name => "tag"
+      
+      @commenter = User.create! valid_user_attributes
+      @comment = @commenter.comments.create! :tag_id => @tag.id, :body => "comment"
+    end
+
+    it "admin can find tag" do
+      admin = User.create! valid_user_attributes
+      admin.has_role("admin")
+      
+      Comment.find_for_user(admin, @comment.id).should == @comment
+    end
+
+    it "commenter can find tag" do
+      Comment.find_for_user(@commenter, @comment.id).should == @comment
+    end
+
+    it "tagger can find tag" do
+      Comment.find_for_user(@tagger, @comment.id).should == @comment
+    end
+
+    it "noone else can find tag" do
+      user = User.create! valid_user_attributes
+      
+      lambda {
+        Comment.find_for_user(user, @comment.id)
+      }.should raise_error(ActiveRecord::RecordNotFound)
+    end
+  end
 end
