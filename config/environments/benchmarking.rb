adfd7d581e9e1538a54894e2600d06181a1fe2c4
Sean Geoghegan <seangeo@gmail.com>
2008-05-14 13:50:38 +0930

    Got railsbench benchmarks working again

--- config/environments/benchmarking.rb
+++ config/environments/benchmarking.rb
@@ -17,4 +17,7 @@ config.action_controller.perform_caching             = true
 # Disable delivery errors if you bad email addresses should just be ignored
 # config.action_mailer.raise_delivery_errors = false
 
+config.action_controller.session_store = :p_store
+config.action_controller.session = {}
+
 ActionController::Base.fragment_cache_store = :file_store, File.join(RAILS_ROOT, 'tmp', 'cache')
