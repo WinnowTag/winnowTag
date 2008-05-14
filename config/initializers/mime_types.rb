c766a9f2b30801305b9479a53af7380232b8c14d
Sean Geoghegan <seangeo@gmail.com>
2008-05-14 18:59:09 +0930

    Atom parameter parser tries feeds and entries.

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -5,8 +5,15 @@ Mime::Type.register "application/atomsvc+xml", :atomsvc
 # Mime::Type.register_alias "text/html", :iphone
 
 ActionController::Base.param_parsers[Mime::Type.lookup("application/atom+xml")] = Proc.new do |body| 
+  
   begin
     { :atom => Atom::Entry.load_entry(body) } 
+  rescue ArgumentError
+    begin
+      { :atom => Atom::Feed.load_feed(body) }
+    rescue Atom::ParseError => ape
+      { :atom_error => ape }
+    end
   rescue Atom::ParseError => ape
     { :atom_error => ape }
   end
