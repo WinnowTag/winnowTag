cf424b8467bce39877e6459f9db5e0518b9b2119
Mark Van Holstyn <mvanholstyn@gmail.com>
2008-05-19 12:56:24 -0400

    Fixed sensitivity slider

--- app/helpers/bias_slider_helper.rb
+++ app/helpers/bias_slider_helper.rb
@@ -15,44 +15,38 @@ module BiasSliderHelper
   # When tag is nil, a disabled slider is shown with the default
   # bias. When tag is :default the default bias will be show, but it will be editable.
   # When tag is a user tag the bias for that tag will be shown and it will be editable.
-  def bias_slider(tag, options = {})    
-    bias = ((tag.is_a?(Tag) ? tag.bias : 1.0) or 1.0)   
+  def bias_slider(tag, options = {})
+    bias_slider_html(tag) + javascript_tag(bias_slider_js(tag, options))
+  end
+  
+  def bias_slider_js(tag, options = {})    
+    bias = tag.bias || 1.0
     # Don't let it go over 1.3
     max_bias_value = [bias, 1.3].max     
     slider_disabled = options[:disabled] ? true : false
     prefix = dom_id(tag, 'slider')
-    variable = "#{prefix}_var"
         
-    js = "var #{variable} = new BiasSlider('#{prefix}_handle', '#{prefix}_track', {" + 
+    js  = "if(!BiasSlider.sliders.#{prefix}) {"
+    js << "BiasSlider.sliders.#{prefix} = new BiasSlider('#{prefix}_handle', '#{prefix}_track', {" + 
               "onChange: function(newBias, slider) {slider.sendUpdate(newBias, slider.options.tag_id);}," +
               "tag_id: #{tag.id}," +
   						"disabled: #{slider_disabled}," +
   						"range: $R(0.9, #{max_bias_value})," +
   						"sliderValue: #{bias}" +
-  				"});\n"
-  	
-    # Set the position of the slider markers
-    [0.9, 1.0, 1.1, 1.2, 1.3].each do |v|
-      js << "$('#{prefix}_#{v.to_s.sub('.', '_')}').setStyle({left: #{variable}.translateToPx(#{v})});\n"
-      
-      unless slider_disabled
-        js << "Event.observe($('#{prefix}_#{v.to_s.sub('.', '_')}'), 'mousedown', function(){#{variable}.setValue(#{v})});\n"
-      end
-    end
-    
-    bias_slider_html(prefix) + javascript_tag(js)
+  				"});"
+  	js << "}"
   end
   
-private
-  def bias_slider_html(prefix)
+  def bias_slider_html(tag)
+    prefix = dom_id(tag, 'slider')
     content_tag 'div', 
       content_tag('div', 
-        content_tag('div', '', :id => "#{prefix}_0_9",    :class => 'bias_marker',         :title => _(:first_slider_marker))  +
-        content_tag('div', '', :id => "#{prefix}_1_0",    :class => 'bias_marker neutral', :title => _(:second_slider_marker)) +
-        content_tag('div', '', :id => "#{prefix}_1_1",    :class => 'bias_marker',         :title => _(:third_slider_marker))  +
-        content_tag('div', '', :id => "#{prefix}_1_2",    :class => 'bias_marker',         :title => _(:fourth_slider_marker)) +
-        content_tag('div', '', :id => "#{prefix}_1_3",    :class => 'bias_marker',         :title => _(:fifth_slider_marker))  +
-        content_tag('div', '', :id => "#{prefix}_handle", :class => 'slider_handle',       :title => _(:slider_handle_tooltip)),
+        content_tag('div', '', :id => "#{prefix}_0_9",    :class => '0_9 bias_marker',         :title => _(:first_slider_marker))  +
+        content_tag('div', '', :id => "#{prefix}_1_0",    :class => '1_0 bias_marker neutral', :title => _(:second_slider_marker)) +
+        content_tag('div', '', :id => "#{prefix}_1_1",    :class => '1_1 bias_marker',         :title => _(:third_slider_marker))  +
+        content_tag('div', '', :id => "#{prefix}_1_2",    :class => '1_2 bias_marker',         :title => _(:fourth_slider_marker)) +
+        content_tag('div', '', :id => "#{prefix}_1_3",    :class => '1_3 bias_marker',         :title => _(:fifth_slider_marker))  +
+        content_tag('div', '', :id => "#{prefix}_handle", :class => 'slider_handle',           :title => _(:slider_handle_tooltip)),
         :id => "#{prefix}_track", :class => 'slider_track'), 
       :class => "slider"
   end
