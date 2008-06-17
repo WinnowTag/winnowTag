54b752fb1df7d7341309c0c138a501247beb7bbd
Mark Van Holstyn <mvanholstyn@gmail.com>
2008-06-17 15:26:06 -0400

    Got rid of control_link helper and one instance of its use

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -41,38 +41,6 @@ module ApplicationHelper
     end
   end
   
-  # Provides some assistance over link_to for special control links
-  #
-  # == New Parameters
-  #   * disabled - if true the link will be disabled
-  #   * requires - a permission expression that if permits?(options[:requires]) evaluates to true
-  #                the link will be rendered, otherwise it wont
-  def control_link(content, url, options = {})
-    html_options = options.delete(:html) || {}
-    css_classes = ["control"]
-    css_classes << html_options[:class] unless html_options[:class].blank?
-    
-    if options[:requires].nil? or options[:requires] == true or (options[:requires].is_a?(String) and permit?(options.delete(:requires)))
-      if options.delete(:remote)
-        
-        if options.delete(:disabled)
-          css_classes << 'disabled'
-        end
-        
-        options[:url] = url
-        options[:condition] = '!this.hasClassName("disabled")'
-        link_to_remote(content, options, html_options.merge(:class => css_classes.join(" ")))
-      else
-        link_to_unless(options.delete(:disabled), content, url, options.merge(:class => css_classes.join(" "))) do |name|
-          css_classes << "disabled"
-          link_to(name, '#', html_options.merge(:class => css_classes.join(" ")))
-        end
-      end
-    elsif options[:alternate]
-      options[:alternate]
-    end
-  end
-  
   def is_admin?
     if @is_admin.nil?
       @is_admin = (current_user.has_role?('admin') == true)
