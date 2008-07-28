bf78c4237588fb28cb178e7294c4169361f1c5a4
Mark Van Holstyn <mvanholstyn@gmail.com>
2008-07-28 13:41:01 -0400

    Fixed drag/drop on sidebar - reverting back to original scripty implementation

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -3,6 +3,10 @@
 // Possession of a copy of this file grants no permission or license
 // to use, modify, or create derivate works.
 // Please visit http://www.peerworks.org/contact for further information.
+
+// This is needed for the sidebar drag/drop
+Position.includeScrollOffsets = true;
+
 function exceptionToIgnore(e) {
   // Ignore this Firefox error because it just occurs when a XHR request is interrupted.
   return e.name == "NS_ERROR_NOT_AVAILABLE"
