7e82af4d0926996444eec61857e1285d8227c39f
Sean Geoghegan <seangeo@gmail.com>
2008-08-05 12:10:12 +0930

    Get feed story working with hmac on remote feeds.

--- stories/feeds.rb
+++ stories/feeds.rb
@@ -6,6 +6,7 @@
 
 require File.dirname(__FILE__) + '/helper'
 require 'active_resource/http_mock'
+require 'spec/mocks'
 
 steps_for(:feeds) do
   Given("I am logged in") do
@@ -20,9 +21,12 @@ steps_for(:feeds) do
   end
   
   Given('a running collector') do
+    now = Time.parse("Thu, 10 Jul 2008 03:29:56 GMT")
+    Time.stub!(:now).and_return(now)
     ActiveResource::HttpMock.respond_to do |http|
-      http.post  "/feeds.xml", {}, {:url => 'http://example.org/feed', :created_by => 'quentin'}.to_xml(:root => 'feed'), 201, 'Location' => '/feeds/23'
-      http.post  "/feeds/23/collection_jobs.xml", {}, 
+      http.post  "/feeds.xml", {"Authorization" => "AuthHMAC winnow_id:HpYILSABqZYTFkEVKfDWZglB7GY=", "Content-Type" => "application/xml"},
+        {:url => 'http://example.org/feed', :created_by => 'quentin'}.to_xml(:root => 'feed'), 201, 'Location' => '/feeds/23'
+      http.post  "/feeds/23/collection_jobs.xml", {"Authorization" => "AuthHMAC winnow_id:rs5dU0B3apzzOe4HnUf7L82wcsA=", "Content-Type" => "application/xml"}, 
                  {:callback_url => 'http://www.example.com/users/quentin/collection_job_results', :created_by => 'quentin'}.to_xml(:root => 'collection_job'), 201, 'Location' => '/feeds/23'
     end
   end
@@ -33,9 +37,11 @@ steps_for(:feeds) do
   
   Then("I am redirected") do
     response.code.should == "302"
+    Time.rspec_reset
   end
 end
 
 with_steps_for(:feeds) do
   run_local_story 'feeds', :type => RailsStory
 end
+
