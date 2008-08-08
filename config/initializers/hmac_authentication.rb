7832ede5807f3cdd7832d2ab9e1435dac44e504c
Sean Geoghegan <seangeo@gmail.com>
2008-08-08 14:19:09 +0930

    Refuse to start in production mode without a credentials file.

--- config/initializers/hmac_authentication.rb
+++ config/initializers/hmac_authentication.rb
@@ -6,6 +6,8 @@ end
 
 if File.exist?(credentials_file)
   HMAC_CREDENTIALS = YAML.load(File.read(credentials_file))
+elsif RAILS_ENV == 'production'
+  raise "Winnow will not start in production mode without providing a credentials file at #{credentials_file}"
 else
   HMAC_CREDENTIALS = {}
 end
