bba8c36f8a7c5ef8387f7edb1481d591e9f4e975
Sean Geoghegan <seangeo@gmail.com>
2008-08-11 13:32:02 +0930

    Freeze auth-hmac version 1.0.0

--- config/environment.rb
+++ config/environment.rb
@@ -37,7 +37,7 @@ Rails::Initializer.run do |config|
   config.gem "hpricot"
   config.gem "bcrypt-ruby", :lib => "bcrypt"
   config.gem "RedCloth", :version => "4.0.1"
-  config.gem "auth-hmac"
+  config.gem "auth-hmac", :version => "1.0.0"
   
   # Only load the plugins named here, in the order given. By default, all plugins 
   # in vendor/plugins are loaded in alphabetical order.
