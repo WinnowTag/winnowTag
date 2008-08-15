9ccc855197500265d7735318af117c4eda96d33a
Sean Geoghegan <seangeo@gmail.com>
2008-08-15 16:07:21 +0930

    Require fiveruns when running on winnow

--- config/environment.rb
+++ config/environment.rb
@@ -40,6 +40,10 @@ Rails::Initializer.run do |config|
   config.gem "tzinfo", :version => "0.3.9"
   config.gem "auth-hmac", :version => "1.0.0"
   
+  if `uname -n` =~ /ds468-1.blueboxgrid.com/
+    config.gem :fiveruns_manage
+  end
+  
   # Only load the plugins named here, in the order given. By default, all plugins 
   # in vendor/plugins are loaded in alphabetical order.
   # :all can be used as a placeholder for all plugins not explicitly named
