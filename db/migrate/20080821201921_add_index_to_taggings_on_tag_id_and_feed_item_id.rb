e324ddbb20dfb9409c0948c815573c49e9deb75f
Sean Geoghegan <seangeo@gmail.com>
2008-08-22 11:29:21 +0930

    Added Kaphan header to migration.

--- db/migrate/20080821201921_add_index_to_taggings_on_tag_id_and_feed_item_id.rb
+++ db/migrate/20080821201921_add_index_to_taggings_on_tag_id_and_feed_item_id.rb
@@ -1,3 +1,10 @@
+# Copyright (c) 2008 The Kaphan Foundation
+#
+# Possession of a copy of this file grants no permission or license
+# to use, modify, or create derivate works.
+# Please visit http://www.peerworks.org/contact for further information.
+
+
 class AddIndexToTaggingsOnTagIdAndFeedItemId < ActiveRecord::Migration
   def self.up
     add_index :taggings, [:tag_id, :feed_item_id]
