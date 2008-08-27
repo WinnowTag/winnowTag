7e326ca7f3085ee84077f4425b4cc92029e10361
Mark Van Holstyn <mvanholstyn@gmail.com>
2008-08-27 15:07:57 -0400

    Moved asset expansion into an initializer

--- config/environment.rb
+++ config/environment.rb
@@ -84,9 +84,4 @@ Rails::Initializer.run do |config|
 
   # Activate observers that should always be running
   config.active_record.observers = :text_indexing_observer
-  
-  config.after_initialize do 
-    ActionView::Helpers::AssetTagHelper.register_javascript_expansion :winnow => ["slider", "element", "cookies", "applesearch", "bias_slider", "timeout", "messages", "labeled_input", "scroll", "classification", "itembrowser", "item", "sidebar", "content"]
-    ActionView::Helpers::AssetTagHelper.register_stylesheet_expansion :winnow => ["winnow", "tables", "slider", "scaffold", "button"]
-  end
 end
---
+++ config/initializers/assets.rb
@@ -0,0 +1,2 @@
+ActionView::Helpers::AssetTagHelper.register_javascript_expansion :winnow => ["slider", "element", "cookies", "applesearch", "bias_slider", "timeout", "messages", "labeled_input", "scroll", "classification", "itembrowser", "item", "sidebar", "content"]
+ActionView::Helpers::AssetTagHelper.register_stylesheet_expansion :winnow => ["winnow", "tables", "slider", "scaffold", "button"]
