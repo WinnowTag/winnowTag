9005a2f542b6f1906b7022d7d10b0b9688747814
Sean Geoghegan <seangeo@gmail.com>
2008-08-28 10:40:06 +0930

    Added migration to drop taggings(tag_id) index.

---
+++ db/migrate/20080828010731_drop_taggings_tag_id_index.rb
@@ -0,0 +1,15 @@
+# Copyright (c) 2008 The Kaphan Foundation
+#
+# Possession of a copy of this file grants no permission or license
+# to use, modify, or create derivate works.
+# Please visit http://www.peerworks.org/contact for further information.
+
+class DropTaggingsTagIdIndex < ActiveRecord::Migration
+  def self.up
+    remove_index(:taggings, :tag_id) rescue nil
+  end
+
+  def self.down
+    add_index(:taggings, :tag_id)
+  end
+end
