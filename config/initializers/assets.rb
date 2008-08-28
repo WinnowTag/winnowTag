d8551110cfdbc268656f2eb89ac6bd3f531fa4c0
Mark Van Holstyn <mvanholstyn@gmail.com>
2008-08-28 12:42:24 -0400

    Renamed itembrowser.js to item_browser.js. Renamed applesearch to apple_search.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,2 +1,2 @@
-ActionView::Helpers::AssetTagHelper.register_javascript_expansion :winnow => ["slider", "element", "cookies", "applesearch", "bias_slider", "timeout", "messages", "labeled_input", "scroll", "classification", "itembrowser", "feed_items_item_browser", "item", "sidebar", "content"]
+ActionView::Helpers::AssetTagHelper.register_javascript_expansion :winnow => ["slider", "element", "cookies", "apple_search", "bias_slider", "timeout", "messages", "labeled_input", "scroll", "classification", "item_browser", "feed_items_item_browser", "item", "sidebar", "content"]
 ActionView::Helpers::AssetTagHelper.register_stylesheet_expansion :winnow => ["winnow", "tables", "slider", "scaffold", "button"]
