4318aa8e67ee7075cb03258f36e3c036957ba3c8
Mark Van Holstyn <mvanholstyn@gmail.com>
2008-10-13 12:53:21 -0400

    stop the selenium server

--- config/selenium.rb
+++ config/selenium.rb
@@ -5,7 +5,7 @@ Selenium::configure do |config|
 
   # Uncomment for remote tests. You will need to run selenium-server.jar manually on the remote machine
   # config.selenium_server_host = '172.16.83.129' # IP Address of the machine in the remote machine
-  config.stop_selenium_server = false
+  config.stop_selenium_server = true
   config.selenium_server_display = ':0'
 
   config.test_server_host = 'test.winnow.local'
