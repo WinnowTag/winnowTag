1fb6ad80fb5049eee616fba3122a85e5471c1355
Mark Van Holstyn <mvanholstyn@gmail.com>
2008-10-15 15:07:11 -0400

    Freeze hpricot

--- config/environment.rb
+++ config/environment.rb
@@ -28,13 +28,13 @@ Rails::Initializer.run do |config|
   config.gem "auth-hmac"
   config.gem "bcrypt-ruby",     :lib => "bcrypt"
   config.gem "fastercsv"
+  config.gem "hpricot"
   config.gem "ratom",           :lib => "atom"
   config.gem "RedCloth"
   config.gem "selenium-client", :lib => "selenium/client"
   config.gem "tzinfo"
 
   # Must be compiled/installed on the target system
-  config.gem "hpricot",     :version => "0.6.161"
   config.gem "libxml-ruby", :version => "0.8.3", :lib => "libxml"
   config.gem "mysql",       :version => "2.7"
       
