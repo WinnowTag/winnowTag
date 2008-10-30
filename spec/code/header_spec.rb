e7d01b2b43590a89fc816cf56e658f8d4b9ec13f
Mark Van Holstyn <mvanholstyn@gmail.com>
2008-10-30 12:54:03 -0400

    Added new sidebar block on public tags for subscription/usage stats.

--- spec/code/header_spec.rb
+++ spec/code/header_spec.rb
@@ -7,8 +7,11 @@ require File.dirname(__FILE__) + '/../spec_helper'
 
 describe "headers" do
   it "all ruby files should have the kaphan foundation header" do
-    vendor = %w[lib/authenticated_system.rb lib/authenticated_test_helper.rb lib/mongrel_health_check_handler.rb 
-                 lib/tasks/cucumber.rake lib/tasks/rspec.rake]
+    vendor = %w[
+      lib/authenticated_system.rb lib/authenticated_test_helper.rb
+      lib/mongrel_health_check_handler.rb 
+      lib/tasks/cucumber.rake lib/tasks/rspec.rake
+    ]
     
     (Dir["{app,lib,db,profiling,spec,stories}/**/*.{rb,rake}"] - vendor).each do |filename|
       filename.should have_ruby_kaphan_header
@@ -19,6 +22,7 @@ describe "headers" do
     vendor = %w[
       public/javascripts/controls.js public/javascripts/dragdrop.js public/javascripts/effects.js public/javascripts/prototype.js
       public/javascripts/slider.js public/javascripts/unittest.js public/javascripts/all.js
+      public/javascripts/tuneup/init.js public/javascripts/tuneup/prototype.js public/javascripts/tuneup/tuneup.js
     ]
     
     (Dir["public/javascripts/**/*.js"] - vendor).each do |filename|
@@ -27,7 +31,10 @@ describe "headers" do
   end
   
   it "all stylesheets files should have the kaphan foundation header" do
-    vendor = %w[public/stylesheets/all.css public/stylesheets/blueprint/screen.css]
+    vendor = %w[
+      public/stylesheets/all.css public/stylesheets/blueprint/screen.css 
+      public/stylesheets/tuneup/tuneup.css
+    ]
     
     (Dir["public/stylesheets/**/*.css"] - vendor).each do |filename|
       filename.should have_stylesheet_kaphan_header
