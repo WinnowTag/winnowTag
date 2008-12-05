4e4fe6a3be6fd2f16fb28d40e23c6fc6f89803a7
Sean Geoghegan <seangeo@gmail.com>
2008-12-05 16:10:53 +1030

    Copy uris to feeds

---
+++ db/migrate/20081205050334_copy_uris_from_collector.rb
@@ -0,0 +1,28 @@
+# Copyright (c) 2008 The Kaphan Foundation
+#
+# Possession of a copy of this file grants no permission or license
+# to use, modify, or create derivate works.
+# Please visit http://www.peerworks.org/contact for further information.
+
+
+class CopyUrisFromCollector < ActiveRecord::Migration
+  # Here we copy the URIs from the collector into Winnow. This needs
+  # to be done to ensure that global uri are bound to the same feed
+  # in both the collector and Winnow.  Once this is done the id columns
+  # in the collector and winnow will be considered separate and 
+  # a feed's collector assigned uri will be the identifying attribute
+  # for a feed.
+  #
+  #
+  # This migration requires the winnow DB user to have access
+  # to the collector database. This should be a simple temporary
+  # permission change.  If there is a case where it isn't we may 
+  # need some additional code to handle importing the new id map.
+  #
+  def self.up
+    execute "update feeds set uri = (select uri from collector.feeds where collector.feeds.url = feeds.via)"
+  end
+
+  def self.down
+  end
+end
