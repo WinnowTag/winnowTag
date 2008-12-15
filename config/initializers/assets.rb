1d01ec5d82607ab6c6c0d8242e988e59ed1c792c
Mark Van Holstyn <mvanholstyn@gmail.com>
2008-12-15 11:07:38 -0500

    Refactor sidebar section inline javascript into sidebar_section.js

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,2 +1,2 @@
-ActionView::Helpers::AssetTagHelper.register_javascript_expansion :winnow => ["slider", "element", "cookies", "apple_search", "bias_slider", "timeout", "messages", "labeled_input", "scroll", "classification", "item_browser", "feed_items_item_browser", "tags_item_browser", "item", "sidebar", "content"]
+ActionView::Helpers::AssetTagHelper.register_javascript_expansion :winnow => ["slider", "element", "cookies", "apple_search", "bias_slider", "timeout", "messages", "labeled_input", "scroll", "classification", "item_browser", "feed_items_item_browser", "tags_item_browser", "item", "sidebar", "sidebar_section", "content"]
 ActionView::Helpers::AssetTagHelper.register_stylesheet_expansion :winnow => ["defaults", "button", "winnow", "tables", "slider", "scaffold"]
