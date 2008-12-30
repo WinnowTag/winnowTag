4a3a4ade7da155c982f6cc6597d9191254ec2839
Mark Van Holstyn <mvanholstyn@gmail.com>
2008-12-30 13:08:40 -0500

    Removing treetop. Require it to be installed.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -22,4 +22,6 @@ config.action_controller.allow_forgery_protection    = false
 config.action_mailer.delivery_method = :test
 
 config.gem :nokogiri
+
+# Must be compiled/installed on the target system
 config.gem :treetop
