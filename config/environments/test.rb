69357e8a7e3d0c90c5a7c8bf0cd4cd2b324cd43d
Mark Van Holstyn <mvanholstyn@gmail.com>
2009-03-02 20:42:32 -0500

    Attempting to get this to load properly on the new CI server

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -22,9 +22,9 @@ config.action_controller.allow_forgery_protection    = false
 config.action_mailer.delivery_method = :test
 
 # Must be compiled/installed on the target system
-config.gem "term-ansicolor", :version => "1.0.3", :lib => "term/ansicolor"
-config.gem "polyglot", :version => "0.2.3", :lib => false
-config.gem "treetop", :version => "1.2.4", :lib => "treetop/runtime"
+# config.gem "term-ansicolor", :version => "1.0.3", :lib => "term/ansicolor"
+# config.gem "polyglot", :version => "0.2.3", :lib => false
+# config.gem "treetop", :version => "1.2.4", :lib => "treetop/runtime"
 
 # Not bundled until we can deploy without test dependencies being built
 # See http://rails.lighthouseapp.com/projects/8994-ruby-on-rails/tickets/1793-make-rake-gemsbuild-respect-railsenv
