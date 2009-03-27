57b3e8ea108a0c94d6cd2016a12478ec117f155b
Sean Geoghegan <seangeo@gmail.com>
2009-03-27 17:36:59 +1030

    Capture index removal errors.

--- db/migrate/20090327060429_optimize_tagging_indexes.rb
+++ db/migrate/20090327060429_optimize_tagging_indexes.rb
@@ -8,13 +8,13 @@ class OptimizeTaggingIndexes < ActiveRecord::Migration
   def self.up
     add_index :taggings, [:feed_item_id, :tag_id, :classifier_tagging], :unique => true
 
-    remove_index :taggings, :name => 'user_tag_item_classifier'
-    remove_index :taggings, :name => 'user_item_tag_classifier'
-    remove_index :taggings, :name => 'index_taggings_on_tag_id_and_feed_item_id'
-    remove_index :taggings, :name => 'index_taggings_on_tag_id_and_classifier_tagging'
-    remove_index :taggings, :name => 'index_taggings_on_tag_id_and_created_on'
-    remove_index :taggings, :name => 'item_tag_strength_classifier'
-    remove_index :taggings, :name => 'index_taggings_on_user_id_and_classifier_tagging'
+    remove_index :taggings, :name => 'user_tag_item_classifier'                         rescue puts("error deleting index")
+    remove_index :taggings, :name => 'user_item_tag_classifier'                         rescue puts("error deleting index")
+    remove_index :taggings, :name => 'index_taggings_on_tag_id_and_feed_item_id'        rescue puts("error deleting index")
+    remove_index :taggings, :name => 'index_taggings_on_tag_id_and_classifier_tagging'  rescue puts("error deleting index")
+    remove_index :taggings, :name => 'index_taggings_on_tag_id_and_created_on'          rescue puts("error deleting index")
+    remove_index :taggings, :name => 'item_tag_strength_classifier'                     rescue puts("error deleting index")
+    remove_index :taggings, :name => 'index_taggings_on_user_id_and_classifier_tagging' rescue puts("error deleting index")
 
     execute "optimize table taggings"
   end
