1ae1ff44a999698bc60581628be60c58e9a5a056
Mark Van Holstyn <mvanholstyn@gmail.com>
2009-05-27 14:05:54 -0400

    Ticket #1038 - fix exception with login_path route and a token.

--- config/routes.rb
+++ config/routes.rb
@@ -105,8 +105,7 @@ ActionController::Routing::Routes.draw do |map|
                 
   map.with_options :controller => "account" do |account_map|
     account_map.edit_account "account/edit/:id", :action => "edit"
-    account_map.login "account/login", :action => "login"
-    account_map.connect "account/login/:code", :action => "login"
+    account_map.login "account/login/:code", :action => "login", :code => nil
     account_map.signup "account/signup", :action => "signup", :conditions => { :method => :post }
     account_map.signup_invite "account/invite", :action => "invite", :conditions => { :method => :post }
     account_map.logout "account/logout", :action => "logout"
