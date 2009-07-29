2b0ab0bef69153caf89ef8a3423e725083e504b7
Craig Demyanovich <cdemyanovich@mutuallyhuman.com>
2009-07-29 15:20:43 -0400

    #1052 - RDocs for TagExclusion

--- app/models/tag_exclusion.rb
+++ app/models/tag_exclusion.rb
@@ -3,6 +3,10 @@
 # Possession of a copy of this file grants no permission or license
 # to use, modify, or create derivate works.
 # Please visit http://www.peerworks.org/contact for further information.
+
+# Represents the fact that a User has blocked all items from a Tag
+# from appearing in his Winnow Items page views. TagExclusion might be
+# considered the opposite of TagSubscription.
 class TagExclusion < ActiveRecord::Base
   belongs_to :user
   belongs_to :tag
