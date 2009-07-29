516dd666522ea83a7dd2c98d9ed17edc8e0a246f
Mark Van Holstyn <mvanholstyn@gmail.com>
2009-07-29 12:44:19 -0400

    access the rails environment via Rails.env. check the rails environment via string inquirer

--- config/initializers/hmac_authentication.rb
+++ config/initializers/hmac_authentication.rb
@@ -1,4 +1,4 @@
-if RAILS_ENV == 'test'
+if Rails.env.test?
   credentials_file = File.join(RAILS_ROOT, 'config', 'hmac_credentials.yml.example')
 else
   credentials_file = File.join(RAILS_ROOT, 'config', 'hmac_credentials.yml')
@@ -6,7 +6,7 @@ end
 
 if File.exist?(credentials_file)
   HMAC_CREDENTIALS = YAML.load(File.read(credentials_file))
-elsif RAILS_ENV == 'production'
+elsif Rails.env.production?
   raise "Winnow will not start in production mode without providing a credentials file at #{credentials_file}"
 else
   HMAC_CREDENTIALS = {}
