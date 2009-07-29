06cb695fdee12f4554f58b6294b5253337853ed9
Craig Demyanovich <cdemyanovich@mutuallyhuman.com>
2009-07-29 15:33:48 -0400

    #1052 - RDocs for TextIndexingObserver

--- app/models/text_indexing_observer.rb
+++ app/models/text_indexing_observer.rb
@@ -3,6 +3,9 @@
 # Possession of a copy of this file grants no permission or license
 # to use, modify, or create derivate works.
 # Please visit http://www.peerworks.org/contact for further information.
+
+# Observes FeedItem instances in order to keep a full-text index of their
+# content up-to-date.
 class TextIndexingObserver < ActiveRecord::Observer
   observe :feed_item
   
