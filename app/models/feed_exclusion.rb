94069186397a60379a061d9e8b9cc5819c90ec7b
Craig Demyanovich <cdemyanovich@mutuallyhuman.com>
2009-07-29 13:33:44 -0400

    #1052 - Updated RDocs for FeedExclusion based on those for FeedSubscription

--- app/models/feed_exclusion.rb
+++ app/models/feed_exclusion.rb
@@ -5,7 +5,8 @@
 # Please visit http://www.peerworks.org/contact for further information.
 
 # Represents the fact that a User has blocked all items from a Feed
-# from appearing in his Winnow Items page views.
+# from appearing in his Winnow Items page views. FeedExclusion might be
+# considered the opposite of FeedSubscription.
 class FeedExclusion < ActiveRecord::Base
   belongs_to :user
   belongs_to :feed
