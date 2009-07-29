5a3c0707d4f8b6c678d00cd8bdbe64e17156eec7
Craig Demyanovich <cdemyanovich@mutuallyhuman.com>
2009-07-29 15:20:55 -0400

    #1052 - RDocs for TagSubscription

--- app/models/tag_subscription.rb
+++ app/models/tag_subscription.rb
@@ -3,6 +3,10 @@
 # Possession of a copy of this file grants no permission or license
 # to use, modify, or create derivate works.
 # Please visit http://www.peerworks.org/contact for further information.
+
+# Represents a User subscribing to a Tag. In other words, the User wants
+# to see content from this Tag. TagSubscription might be considered the
+# opposite of TagExclusion.
 class TagSubscription < ActiveRecord::Base
   belongs_to :user
   belongs_to :tag
