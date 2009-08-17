cf4ef412a810b7bcf53ede9325d111d678a83ff6
Mark Van Holstyn <mvanholstyn@gmail.com>
2009-08-17 11:54:51 -0400

    Fix deployment mailer to run in the proper rails env

--- config/deploy.rb
+++ config/deploy.rb
@@ -120,7 +120,7 @@ desc "Notify the list of deployment"
 task :send_notification do
   mail_comment = comment rescue mail_comment = "None provided."
   # Run it on the server so we know we have a good email configuration
-  run %Q(cd #{current_path} && script/runner -e production 'Notifier.deliver_deployed("http://#{domain}", "#{repository}", "#{revision}", "#{ENV['USER']}", "#{mail_comment}")')
+  run %Q(cd #{current_path} && script/runner -e #{rails_env} 'Notifier.deliver_deployed("http://#{domain}", "#{repository}", "#{revision}", "#{ENV['USER']}", "#{mail_comment}")')
 end
 
 after 'deploy:update_code', :copy_config
