ba1a69525dbd721691350208b656ba25283182f4
Mark Van Holstyn <mvanholstyn@gmail.com>
2009-08-20 16:00:11 -0400

    remove old, unused readings tables.

---
+++ db/migrate/20090814200356_drop_read_items_and_message_readings.rb
@@ -0,0 +1,27 @@
+# Copyright (c) 2008 The Kaphan Foundation
+#
+# Possession of a copy of this file grants no permission or license
+# to use, modify, or create derivate works.
+# Please visit http://www.peerworks.org/contact for further information.
+class DropReadItemsAndMessageReadings < ActiveRecord::Migration
+  def self.up
+    drop_table :message_readings
+    drop_table :read_items
+  end
+
+  def self.down
+    create_table "read_items", :force => true do |t|
+      t.integer  "user_id"
+      t.integer  "feed_item_id"
+      t.datetime "created_at"
+      t.datetime "updated_at"
+    end
+    
+    create_table "message_readings", :force => true do |t|
+      t.integer  "message_id"
+      t.integer  "user_id"
+      t.datetime "created_at"
+      t.datetime "updated_at"
+    end
+  end
+end
