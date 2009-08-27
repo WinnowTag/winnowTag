8eb55f3b7eb302f4e9b7ae2c862422a01c1e1e35
Mark Van Holstyn <mvanholstyn@gmail.com>
2009-08-27 16:50:39 -0400

    adding missing headers

--- db/migrate/20090827194118_update_feeds_sort_title_to_strip_everything_except_letters_and_numbers.rb
+++ db/migrate/20090827194118_update_feeds_sort_title_to_strip_everything_except_letters_and_numbers.rb
@@ -1,3 +1,8 @@
+# Copyright (c) 2008 The Kaphan Foundation
+#
+# Possession of a copy of this file grants no permission or license
+# to use, modify, or create derivate works.
+# Please visit http://www.peerworks.org/contact for further information.
 class UpdateFeedsSortTitleToStripEverythingExceptLettersAndNumbers < ActiveRecord::Migration
   def self.up
     Feed.find_each do |feed|
