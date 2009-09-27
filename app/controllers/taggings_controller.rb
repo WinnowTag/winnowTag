7111cd7dd01f3621ab65e73415056cc346d5fbd5
Mark Van Holstyn <mvanholstyn@gmail.com>
2009-09-27 12:54:05 -0400

    restrict the transaction to only be around the important save call

--- app/controllers/taggings_controller.rb
+++ app/controllers/taggings_controller.rb
@@ -22,17 +22,16 @@ class TaggingsController < ApplicationController
   # is not already in the users sidebar, it will be added.
   def create
     tag = Tag(current_user, params[:tagging][:tag])
-    Tagging.transaction do
-      @tagging = Tagging.new(params[:tagging].merge(:tag => tag, :user => current_user))
-      if @tagging.save
-        unless tag.show_in_sidebar?
-          tag.update_attribute(:show_in_sidebar, true)
-        end
-        respond_to :json
-      else
-        respond_to do |format|
-          format.json { render :action => "error.json.erb" }
-        end
+    @tagging = Tagging.new(params[:tagging].merge(:tag => tag, :user => current_user))
+    # Save the tagging within a transaction to avoid errors from duplicate taggings
+    if Tagging.transaction { @tagging.save }
+      unless tag.show_in_sidebar?
+        tag.update_attribute(:show_in_sidebar, true)
+      end
+      respond_to :json
+    else
+      respond_to do |format|
+        format.json { render :action => "error.json.erb" }
       end
     end
   end
