8f503c85a63d4df846d03cdc543fd36da7e758f0
Craig Demyanovich <cdemyanovich@mutuallyhuman.com>
2009-10-02 17:06:21 -0400

    ticket #1057 - strip name from email addresses, and use the name to set first and last name if they're blank

---
+++ db/migrate/20091002202537_remove_name_from_email.rb
@@ -0,0 +1,34 @@
+# Copyright (c) 2008 The Kaphan Foundation
+#
+# Possession of a copy of this file grants no permission or license
+# to use, modify, or create derivate works.
+# Please visit http://www.peerworks.org/contact for further information.
+
+class RemoveNameFromEmail < ActiveRecord::Migration
+  
+  class User < ActiveRecord::Base; end
+  
+  def self.up
+    regex = /(\w+) (.+) <(.+)>/i
+    User.find_each(:conditions => ["email LIKE ?", "%<%"]) do |user|
+      begin
+        md = regex.match(user.email)
+        
+        user.email = md[3]
+        user.firstname = md[1] if user.firstname.blank?
+        user.lastname = md[2] if user.lastname.blank?
+        
+        changes = user.changes
+        user.save!
+        
+        say "User ##{user.id} changes: #{changes.inspect}"
+      rescue => e
+        say "Error removing name from email for user ##{user.id} - #{e.class.name}: #{e.message}"
+      end
+    end
+  end
+
+  def self.down
+    # nothing to do
+  end
+end
