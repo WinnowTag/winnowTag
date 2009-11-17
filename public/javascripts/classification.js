WinnowTag/winnowTag

commit 03d24bf5454f9dc9848bbdfbb1c81cc8f0eba9d4

#1052 - review documentation for JS files, add some documentation, factor out some duplication

public/javascripts/classification.js
@@ -8,7 +8,7 @@
  *  - onStarted
  *  - onStartProgressUpdater
  *  - onProgressUpdated
- *  - onCancelled
+ *  - onReset
  *  - onFinished
  */
 var Classification = Class.create({
@@ -56,7 +56,7 @@ var Classification = Class.create({
       
       onFinished: function() {
         this.classification_progress.hide();
-        this.notify("Reset");
+        this.notify("Finished");
         this.progress_title.update(I18n.t("winnow.javascript.classifier.progress_bar.finish"));
         this.disableClassification();
         if(confirm(I18n.t("winnow.javascript.classifier.progress_bar.reload"))) {
@@ -112,7 +112,7 @@ var Classification = Class.create({
           this.startProgressUpdater();
         } else {
           Message.add('error', transport.responseJSON);
-          this.notify('Cancelled');  
+          this.notify('Reset');
         }
       }.bind(this),
       onTimeout: function() {

public/javascripts/element.js
@@ -5,6 +5,8 @@
 // Please visit http://www.peerworks.org/contact for further information.
 Element.addMethods({
   insertInOrder: function(container, sibling_value_selector, element_html, element_value) {
+    
+    // Strip the attribute name (if present) from the selector and store it separately
     var sibling_value_selector_parts = sibling_value_selector.split("@");
     sibling_value_selector = sibling_value_selector_parts[0];
     var sibling_value_selector_attribute = sibling_value_selector_parts[1];
@@ -21,6 +23,7 @@ Element.addMethods({
         } else {
           sibling_value = sibling_value_element && sibling_value_element.innerHTML.unescapeHTML();
         }
+        
         if(sibling_value.toLowerCase() > element_value.toLowerCase()) {
           element.insert({ before: element_html });
           needToInsert = false;

public/javascripts/feed_items_item_browser.js
@@ -3,6 +3,13 @@
 // Possession of a copy of this file grants no permission or license
 // to use, modify, or create derivate works.
 // Please visit http://www.peerworks.org/contact for further information.
+
+// Manages the list of feed items shown on the Items page. Provides:
+//   * toggling for switching among summary and detail view of each item
+//   * marking items read/unread
+//   * navigation using keyboard shortcuts
+//   * filtering by tag or feed
+
 var FeedItemsItemBrowser = Class.create(ItemBrowser, {
   initialize: function($super, name, container, options) {
     $super(name, container, options);
@@ -365,29 +372,7 @@ var FeedItemsItemBrowser = Class.create(ItemBrowser, {
     var click_event = this.toggleSetFilters.bind(this, {tag_ids: tag_id});
     link.observe("click", click_event);
 
-    if(tag.hasClassName("draggable")) {
-      Draggables.addObserver({
-        onStart: function(eventName, draggable, event) {
-          if(draggable.element == tag) {
-            link.stopObserving("click", click_event);
-          }
-        },
-        onEnd: function(eventName, draggable, event) {
-          if(draggable.element == tag) {
-            setTimeout(function() {
-              link.observe("click", click_event);
-            }, 1);
-          }
-        }
-      });
-
-      new Draggable(tag, { 
-        ghosting: true, revert: true, scroll: 'sidebar', 
-        reverteffect: function(element, top_offset, left_offset) {
-          new Effect.Move(element, { x: -left_offset, y: -top_offset, duration: 0 });
-        }
-      });
-    }
+    this.makeDraggable(tag, link, click_event);
   },
   
   bindFeedFiltersEvents: function() {
@@ -402,29 +387,37 @@ var FeedItemsItemBrowser = Class.create(ItemBrowser, {
     var click_event = this.toggleSetFilters.bind(this, {feed_ids: feed_id});
     link.observe("click", click_event);
     
-    if(feed.hasClassName("draggable")) {
+    this.makeDraggable(feed, link, click_event);
+  },
+  
+  makeDraggable: function(tag_or_feed, link, click_event) {
+    if(tag_or_feed.hasClassName("draggable")) {
       Draggables.addObserver({
         onStart: function(eventName, draggable, event) {
-          if(draggable.element == feed) {
+          if(draggable.element == tag_or_feed) {
             link.stopObserving("click", click_event);
           }
         },
         onEnd: function(eventName, draggable, event) {
-          if(draggable.element == feed) {
+          if(draggable.element == tag_or_feed) {
             setTimeout(function() {
               link.observe("click", click_event);
             }, 1);
           }
         }
       });
-    
-      new Draggable(feed, { 
-        ghosting: true, revert: true, scroll: 'sidebar', 
-        reverteffect: function(element, top_offset, left_offset) {
-          new Effect.Move(element, { x: -left_offset, y: -top_offset, duration: 0 });
-        }
-      });
+      
+      this.draggableFor(tag_or_feed);
     }
+  },
+  
+  draggableFor: function(tag_or_feed) {
+    new Draggable(tag_or_feed, {
+      ghosting: true, revert: true, scroll: 'sidebar',
+      reverteffect: function(element, top_offset, left_offset) {
+        new Effect.Move(element, { x: -left_offset, y: -top_offset, duration: 0 });
+      }
+    });
   },
 
   bindTextFilterEvents: function() {

public/javascripts/item_browser.js
@@ -3,6 +3,10 @@
 // Possession of a copy of this file grants no permission or license
 // to use, modify, or create derivate works.
 // Please visit http://www.peerworks.org/contact for further information.
+
+// Base class for managing a list of items. More items are added to the list
+// as the user scrolls down, eliminating the need for explicit paging. Sorting
+// and filtering are also handled here.
 var ItemBrowser = Class.create({
   initialize: function(name, container, options) {
     this.options = {

public/javascripts/labeled_input.js
@@ -3,6 +3,8 @@
 // Possession of a copy of this file grants no permission or license
 // to use, modify, or create derivate works.
 // Please visit http://www.peerworks.org/contact for further information.
+
+// Manages the showing/hiding of example text for input controls.
 document.observe('dom:loaded', function() {
   $$("input.example[type=text]").each(function(element) {
     var example_value = element.value;

public/javascripts/tags_item_browser.js
@@ -3,6 +3,8 @@
 // Possession of a copy of this file grants no permission or license
 // to use, modify, or create derivate works.
 // Please visit http://www.peerworks.org/contact for further information.
+
+// Manages the list of tags shown on the My Tags and Public Tags pages.
 var TagsItemBrowser = Class.create(ItemBrowser, {
   initializeTag: function(tag) {
     tag = $(tag);

commit ba9c370bed5872c9d80888477f27c0088d5ca6fb

#1121 - Fixed disappearing autotag button.
Looks like this was broken by 03d24bf5454f9dc9848bbdfbb1c81cc8f0eba9d4. Not sure what the purpose of that change was but I think we need to figure out a good way to incorporate the classifier into the CI server tests... eventually.

public/javascripts/classification.js
@@ -60,7 +60,7 @@ var Classification = Class.create({
       
       onFinished: function() {
         this.classification_progress.hide();
-        this.notify("Finished");
+        this.notify("Reset");
         this.progress_title.update(I18n.t("winnow.javascript.classifier.progress_bar.finish"));
         this.disableClassification();
         if(confirm(I18n.t("winnow.javascript.classifier.progress_bar.reload"))) {