50f97f9eb6ca12da33c64b25a078b9553aa28afe
Sean Geoghegan <seangeo@gmail.com>
2010-02-16 16:15:19 +1030

    More context menu tests

--- spec/selenium/tag_context_menu_spec.rb
+++ spec/selenium/tag_context_menu_spec.rb
@@ -5,7 +5,7 @@
 # Please visit http://www.peerworks.org/contact for further information.
 require File.dirname(__FILE__) + '/../spec_helper'
 
-describe "Tags" do
+describe "Tag Context menu" do
   before(:each) do
     @user = Generate.user!
     @tag1 = Generate.tag! :user => @user, :bias => 0
@@ -54,6 +54,44 @@ describe "Tags" do
       page.click("css=#tag_#{@tag1.id} div.context_menu_button")
       page.is_visible("css=#tag_context_menu").should be_true
       page.is_element_present("css=#tag_context_menu.public").should be_false
+    end
+  end
+  
+  describe "destroy tag" do
+    it "should delete a tag" do
+      page.click("css=#tag_#{@tag1.id} div.context_menu_button")
+      page.click("delete_menu_item")
+      page.should be_confirmation
+      page.confirmation
+      page.wait_for :wait_for => :ajax
+      page.is_element_present("tag_#{@tag1.id}").should be_false
+      lambda{ Tag.find(@tag1.id) }.should raise_error
+    end
+    
+    it "should unsubscribe from a public tag" do
+      page.click("css=#tag_#{@tag2.id} div.context_menu_button")
+      page.click("delete_menu_item")
+      page.should be_confirmation
+      page.confirmation
+      page.wait_for :wait_for => :ajax
+      page.is_element_present("tag_#{@tag2.id}").should be_false
+      lambda{ Tag.find(@tag2.id) }.should_not raise_error
+    end
+  end
+  
+  describe "renaming" do
+    it "should rename owned tag" do
+      page.click("css=#tag_#{@tag1.id} div.context_menu_button")
+      page.answer_on_next_prompt("renamed tag")
+      page.click("rename_menu_item")
+      page.wait_for :wait_for => :ajax
+      Tag.find(@tag1.id).name.should == "renamed tag"
+    end
+    
+    it "should not show prompt for subscribed public tag" do
+      page.click("css=#tag_#{@tag2.id} div.context_menu_button")
+      page.click("rename_menu_item")
+      Tag.find(@tag2.id).name.should == @tag2.name
     end
   end
 end
