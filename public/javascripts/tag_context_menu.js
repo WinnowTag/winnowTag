90945347e7ea39d80d85a0b739465571a6e946bd
Sean Geoghegan <seangeo@gmail.com>
2010-02-16 13:41:13 +1030

    Added rename action to tag context menu.

--- public/javascripts/tag_context_menu.js
+++ public/javascripts/tag_context_menu.js
@@ -15,8 +15,13 @@ var TagContextMenu = Class.create({
     activeMenu = this;
     this.button = button;
     this.menu = $("tag_context_menu");
+    this.tag_id = this.button.getAttribute("tag-id");
+    this.path = "/tags/" + this.tag_id;
     
-    this.tag_id = tag_id;
+    if (this.isSubscribedPublicTag()) {
+      this.menu.addClassName("public");
+    }
+    
     this.tag_li = $("tag_" + tag_id);
     this.tag_li.addClassName("menu-up");
     
@@ -25,8 +30,16 @@ var TagContextMenu = Class.create({
     this.menu.show();
   },
   
+  isSubscribedPublicTag: function() {
+    return $$("li#tag_" + this.tag_id + ".tag.public").length != 0;
+  },
+  
+  getName: function() {
+    return $("name_tag_" + this.tag_id).innerHTML;
+  },
+  
   positionMenu: function() {
-    var topPosition = this.button.cumulativeOffset()[1] + this.button.getHeight() + 2;
+    var topPosition = this.button.cumulativeOffset()[1] - this.button.cumulativeScrollOffset()[1] + this.button.getHeight() + 2;
     var contextHeight = this.menu.getHeight();
     var viewportHeight = document.viewport.getHeight();
     
@@ -40,13 +53,68 @@ var TagContextMenu = Class.create({
   registerHandlers: function() {
     this.destroyHandler = this.destroy.bind(this);
     
+    this.clickHandler = this.click.bindAsEventListener(this);
+    
+    this.menu.select("li").each(function(item) {
+      Event.observe(item, "click", this.clickHandler);
+    }.bind(this));
+    
     Event.observe(document, "click", this.destroyHandler);
+  },
+  
+  click: function(event) {
+    var element = event.findElement("li");
+    var id = element.getAttribute("id");
+    var action = "";
+    
+    if ((match = /(.*)_menu_item/.exec(id)) != null) {
+      action = match[1];
+    }
+    
+    if (this[action]) {
+      this[action]();
+    } else {
+      alert(action + " not implemented!!");
+    }
+  },
+  
+  rename: function() {
+    if (!this.isSubscribedPublicTag()) {
+      var newName = prompt("Please enter a new name for the tag:", this.getName());
+      if (newName && newName != this.getName()) {
+        new Ajax.Request(this.path, {
+              parameters: { 'tag[name]': newName },
+              method: 'put',
+              requestHeaders: { Accept: 'application/json' },
+              onSuccess: function(response) {
+                var data = response.responseJSON;
+                $$(".tag_" + this.tag_id).each(function(e) {
+                  e.down('.name').update(data.name);
+                  e.up().insertInOrder('.name', e, data.name);
+                  new Effect.ScrollToInDiv($("tag_container"), e, 12);
+                  new Effect.Highlight(e, {queue: 'end', endcolor: "#FEF3BB"});
+                });
+              }.bind(this)
+            });
+      }
+    }
+  },
+  
+  delete: function() {
+    
   },
   
   destroy: function() {
     this.menu.hide();
     this.tag_li.removeClassName("menu-up");
+    this.menu.removeClassName("public");
+    
     Event.stopObserving(document, "click", this.destroyHandler);
+    
+    this.menu.select("li").each(function(item) {
+      Event.stopObserving(item, "click", this.clickHandler);
+    }.bind(this));
+    
     if (this == activeMenu) {
       activeMenu = null;
     }
