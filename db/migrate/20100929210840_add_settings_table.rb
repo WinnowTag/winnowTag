c2f7571d9be40487b132a13047bbe08754016ce0
Don Sawtelle <sawtelle@stonecutter.com>
2010-09-29 18:08:29 -0700

    Migration for application settings.

--- db/migrate/20100929210840_add_settings_table.rb
+++ db/migrate/20100929210840_add_settings_table.rb
@@ -1,3 +1,9 @@
+# Copyright (c) 2008 The Kaphan Foundation
+#
+# Possession of a copy of this file grants no permission or license
+# to use, modify, or create derivative works.
+# Please visit http://www.peerworks.org/contact for further information.
+
 class AddSettingsTable < ActiveRecord::Migration
   def self.up
     create_table :settings, :force => true do |t|
