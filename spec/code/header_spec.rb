2f1f9f5e644cf961eb9691bb42c82dbb428ae8a0
Don Sawtelle <sawtelle@stonecutter.com>
2011-06-26 15:51:00 -0700

    Install MIT License, etc.

--- spec/code/header_spec.rb
+++ spec/code/header_spec.rb
@@ -1,8 +1,27 @@
-# Copyright (c) 2008 The Kaphan Foundation
+# General info: http://doc.winnowtag.org/open-source
+# Source code repository: http://github.com/winnowtag
+# Questions and feedback: contact@winnowtag.org
 #
-# Possession of a copy of this file grants no permission or license
-# to use, modify, or create derivative works.
-# Please visit http://www.peerworks.org/contact for further information.
+# Copyright (c) 2007-2011 The Kaphan Foundation
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe "kaphan foundation header" do
@@ -22,7 +41,7 @@ describe "kaphan foundation header" do
   
   it "should be required on all javascript files" do
     vendor = %w[
-      public/javascripts/controls.js public/javascripts/dragdrop.js public/javascripts/effects.js public/javascripts/prototype.js
+      public/javascripts/controls.js public/javascripts/dragdrop.js public/javascripts/effects.js public/javascripts/prototype.js public/javascripts/prototype_1.7.js
       public/javascripts/slider.js public/javascripts/unittest.js public/javascripts/all.js public/javascripts/locales.js
       public/javascripts/placeholder.js public/javascripts/google_analytics_proxy.js
     ]
